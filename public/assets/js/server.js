fed5f9a59bd2e56e6034ee1ced31a8e36f428cb9
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-05-09 17:39:22 -0400

    html hosted locally

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 // =============================================================
 const express = require('express');
+const path = require("path");
 
 // Express Configuration
 // =============================================================
@@ -8,8 +9,11 @@ const express = require('express');
 // Creating an express server
 app = express();
 
+// Code for hosting files in the 'public' directory
+app.use(express.static("public"));
+
 // Setting an initial port
-const PORT = process.env.port || 8000
+const PORT = process.env.port || 8070
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -20,8 +24,18 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // ================================================================================
 
-require("./apiRoutes")(app);
+// require("./apiRoutes")(app);
 require("./htmlRoutes")(app);
+
+// app.get("/notes", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/notes"));
+// });
+
+// // If no matching route is found default to home
+// app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "../../index.html"));
+// });
+
 
 // Listener
 app.listen(PORT, function () {
