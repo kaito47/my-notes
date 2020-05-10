e1d009183bb6cf54d246795a902f1ebd9b02a14b
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-05-10 17:27:58 -0400

    changes html route file paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,12 +12,12 @@ module.exports = function (app) {
     // In each of the below cases the user is shown an HTML page of content
     // ---------------------------------------------------------------------------
     app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, '/public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
     // If no matching route is found default to home
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
 };
